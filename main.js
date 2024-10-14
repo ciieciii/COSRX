100ea7cd4861612b38db67c20f21c4cfcc24c73d
ciieciii <tanayajesika@gmail.com>
2024-10-14 14:39:36 +0700

    bahimuda

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// HOME SWIPER
+
+var swiper = new Swiper(".home-swiper" , {
+    spaceBetween: 85,
+    centeredSlides: true,
+    // autoplay: {
+    //    delay:2500,
+    //    disableOnIntereaction: false,
+    //},
+    pagination: {
+        el: ".swiper-pagination",
+        clickkable: true,
+    },
+});
---
+++ script.js
@@ -0,0 +1,14 @@
+
+// HOME SWIPER
+
+var swiper = new Swiper(".home-mySwiper", {
+    spaceBetween: 85,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
